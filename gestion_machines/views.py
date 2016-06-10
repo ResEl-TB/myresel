22a15ae0855b9d7cae29765acd9eb405c68a5e7a
Morgan ROBIN <morgan@robin.wf>
2016-06-10 22:59:21 +0200

    Creation des vues

--- gestion_machines/views.py
+++ gestion_machines/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class Reactivation(TemplateView):
+	pass
+
+class Ajout(TemplateView):
+	pass
+
+class Campus(TemplateView):
+	pass
