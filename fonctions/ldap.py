972891aca711eabe5187f95db1d97073490ce766
Morgan ROBIN <morgan@robin.wf>
2016-06-10 23:37:42 +0200

    Fonction de recherche dans le LDAP

---
+++ fonctions/ldap.py
@@ -0,0 +1,20 @@
+from ldap3 import Server, Connection
+
+LDAP = 'ldap.resel.fr'
+
+def search(dn, query):
+    """ Fonction pour rechercher dans le ldap une entrée particulière 
+        
+        - dn : le DN dans lequel faire la recherche
+        - query : la recherche en elle-même (uid, host, ip, mac, etc.)
+    """
+
+    l = Connection(Server(LDAP, use_ssl = True), auto_bind = True)
+
+    # On effectue la recherche des machines actives sur Brest
+    l.search(dn, query)
+    res = l.entries
+    l.unbind()
+
+    # On retourne les résultats
+    return res
