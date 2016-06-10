86fe851e8ce6a22a2b5d8307074a548ac7e2c083
Morgan ROBIN <morgan@robin.wf>
2016-06-10 23:06:50 +0200

    Ajout de la partie contact

--- inscription/urls.py
+++ inscription/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = [
     url(r'^machines/', include('gestion_machines.urls', namespace = 'gestion-machines')),
     url(r'^personnes/', include('gestion_personnes.urls', namespace = 'gestion-personnes')),
+    url(r'^contact/', include('contact.urls', namespace = 'contact'))
 ]
