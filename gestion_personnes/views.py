bec77f2eadd638c548a3405ea349c2e13531ef60
Morgan ROBIN <morgan@robin.wf>
2016-06-11 00:15:10 +0200

    Modif de la vue Inscription

--- gestion_personnes/views.py
+++ gestion_personnes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 from fonctions import ldap
 
 # Pour la traduction - sert à marquer les chaînes de caractères à traduire
@@ -12,10 +15,22 @@ class Inscription(TemplateView):
     C'est cette vue qui créer la fiche LDAP de l'user
     On lui affiche le réglement intérieur, et un formulaire pour remplir les champs LDAP
     """
+    template_name = 'gestion_personnes/inscription.html'
+    form_class = InscriptionForm
 
-    rech = ldap.search(DN_PEOPLE, "(&(uid=%s))" % self.request.user)
-    if len(rech) != 0:
-        # L'utilisateur est déjà présent dans le LDAP, on le redirige sur une page d'erreur
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(Inscription, self).dispatch(*args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        rech = ldap.search(DN_PEOPLE, '(&(uid=%s))' % request.user)
+        if len(rech) != 0:
+            # L'utilisateur est déjà présent dans le LDAP, on le redirige sur une page d'erreur
+            pass
+
+        # On affiche le règlement intérieur
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        # On ajoute la personne au LDAP
         pass
-
-    # On ajoute l'utilisateur au LDAP
