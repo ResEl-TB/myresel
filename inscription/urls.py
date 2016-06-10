c4a3462b639d251a9f0ac3994e4710e728779052
Morgan ROBIN <morgan@robin.wf>
2016-06-10 22:52:08 +0200

    Ajout des urls des differentes applis

--- inscription/urls.py
+++ inscription/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^machines/', include('gestion_machines.urls', namespace = 'gestion-machines')),
+    url(r'^personnes/', include('gestion_personnes.urls', namespace = 'gestion-personnes')),
 ]
