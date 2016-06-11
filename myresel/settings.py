596f07b234496c3fcf947bfa1b86743c0cf93ff8
Morgan ROBIN <morgan@robin.wf>
2016-06-12 00:17:19 +0200

    Modifs des settings pour preciser qu'on travaille en FR

--- myresel/settings.py
+++ myresel/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
