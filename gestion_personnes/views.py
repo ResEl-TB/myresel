42fa7c1f79b78e98304f408dcd2a256172344671
Morgan ROBIN <morgan@robin.wf>
2016-06-12 00:07:12 +0200

    Ecriture de la vue d'inscription d'une personne

--- gestion_personnes/views.py
+++ gestion_personnes/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.core.mail import EmailMessage
+from django.http import HttpResponseRedirect
 
-from fonctions import ldap
+from fonctions import ldap, generic
 from .forms import *
 
 # Pour la traduction - sert à marquer les chaînes de caractères à traduire
@@ -19,10 +21,6 @@ class Inscription(TemplateView):
     template_name = 'gestion_personnes/inscription.html'
     form_class = InscriptionForm
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(Inscription, self).dispatch(*args, **kwargs)
-
     def get(self, request, *args, **kwargs):
         form = self.form_class()
         return render(request, self.template_name, {'form': form})
@@ -31,8 +29,41 @@ class Inscription(TemplateView):
         form = self.form_class(request.POST)
 
         if form.is_valid():
-            # On process les données
+            import time
+            year = generic.get_year()
 
-            return 
+            # Mise en forme du DN et des attributs de la fiche LDAP
+            dn = 'uid=%s,ou=people,dc=maisel,dc=enst-bretagne,dc=fr' % form.cleaned_data['pseudo']
+            object_class = ['genericPerson','enstbPerson','reselPerson', 'maiselPerson']
+            attributes = {
+                'uid': form.cleaned_data['pseudo'],
+                'firstname': form.cleaned_data['prenom'],
+                'lastname': form.cleaned_data['nom'],
+                'mail': form.cleaned_data['mail'],
+                'anneeScolaire': '{}/{}'.format(year, year+1),
+                'dateInscr': time.strftime('%Y%m%d%H%M%S') + 'Z',
+                'objectClass': ['genericPerson','enstbPerson','reselPerson', 'maiselPerson'],
+                'campus': generic.get_campus(request.META['HTTP_X_FORWARDED_FOR']),
+                'userPassword': generic.hash_passwd(form.cleaned_data['mot_de_passe']),
+                'ntPassword': generic.hash_to_ntpass(form.cleaned_data['mot_de_passe']),
+                'batiment': form.cleaned_data['batiment'],
+                'roomNumber': form.cleaned_data['chambre'],
+                'mobile': form.cleaned_data['telephone']
+            }
+
+            # Ajout de la fiche au LDAP
+            ldap.add(dn, object_class, attributes)
+
+            # Inscription de la personne à la ML campus
+            mail = EmailMessage(
+                subject = "SUBSCRIBE campus {} {}".format(form.cleaned_data['prenom'], form.cleaned_data['nom']),
+                body = "Inscription automatique de {} a campus".format(form.cleaned_data['pseudo']),
+                from_email = form.cleaned_data['mail'],
+                reply_to = ["listmaster@resel.fr"],
+                to = ["sympa@resel.fr"],
+            )
+            mail.send()
+
+            return HttpResponseRedirect(""" Insérer lien de redirection ici """)
 
         return render(request, self.template_name, {'form': form})
