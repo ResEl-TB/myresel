9a0e25070a59b454344aca2327a3cdc912858c4b
Morgan ROBIN <morgan@robin.wf>
2016-06-12 00:31:07 +0200

    Correction de 'inscription' en 'myresel'

--- myresel/urls.py
+++ myresel/urls.py
@@ -1,4 +1,4 @@
-"""inscription URL Configuration
+"""myresel URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
