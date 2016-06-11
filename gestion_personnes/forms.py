d98e5b28c6d76a6fd38d00fd5c4a0c70adb51a93
Morgan ROBIN <morgan@robin.wf>
2016-06-11 20:14:01 +0200

    Formulaire d'inscription d'une personne

--- gestion_personnes/forms.py
+++ gestion_personnes/forms.py
@@ -1,17 +1,16 @@
 from django import forms
-from fonctions.ldap import search_ecole
+
+# Pour la traduction - sert à marquer les chaînes de caractères à traduire
+from django.utils.translation import ugettext_lazy as _
 
 class InscriptionForm(forms.Form):
-    nom = forms.CharField()
-    prenom = forms.CharField()
-    mail = forms.EmailField()
-    mot_de_passe = forms.CharField(widget = forms.PasswordInput)
+    BATIMENTS = [(i, 'I%d' % i) for i in range(1, 13)]
 
-    def __init__(self, uid):
-        super().__init__()
-        if len(search_ecole(uid)) != 0:
-            # Utilisateur présent dans le LDAP école
-            user = search_ecole(uid)[0]
-            self.nom = forms.CharField(initial = user.cn[0].split(' ')[0])
-            self.prenom = forms.CharField(initial = user.cn[0].split(' ')[1])
-            self.mail = forms.EmailField(initial = user.mail[0])    
+    pseudo = forms.CharField(label = _("Pseudo"))
+    nom = forms.CharField(label = _("Nom"))
+    prenom = forms.CharField(label = _("Prénom"))
+    mail = forms.EmailField(label = _("Adresse mail"))
+    batiment = forms.ChoiceField(label = _("Bâtiment"), choices = BATIMENTS)
+    chambre = forms.IntegerField(label = _("Chambre"), min_value = 0, max_value = 330)
+    telephone = forms.RegexField(label = _("Numéro de téléphone"), regex = r'^0[6-7]([0-9]{2}){4}$')
+    mot_de_passe = forms.CharField(widget = forms.PasswordInput) 
