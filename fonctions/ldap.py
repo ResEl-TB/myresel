0a01ea362d009b76074ab5590955ac79f5214170
Morgan ROBIN <morgan@robin.wf>
2016-06-12 00:07:36 +0200

    Ecriture de la fonction d'ajout de fiche au LDAP

--- fonctions/ldap.py
+++ fonctions/ldap.py
@@ -14,3 +14,10 @@ def search(dn, query):
         res = l.entries
     l.unbind()
     return res
+
+def add(dn, object_class, attributes):
+    """ Fonction qui ajoute une fiche au LDAP """
+
+    l = Connection(Server(LDAP, use_ssl = True), user = DN_ADMIN, password = PASSWD_ADMIN)
+    l.add(dn, object_class, attributes)
+    l.unbind()
