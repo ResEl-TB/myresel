34cc00ca28eb28419eeab7eeb432bcb83cbcda49
Morgan ROBIN <morgan@robin.wf>
2016-06-12 00:08:03 +0200

    Ajout des contantes pour se connecter en admin au LDAP et ajouter une fiche

--- inscription/constantes.py
+++ inscription/constantes.py
@@ -1,5 +1,6 @@
+# Constantes relatives au LDAP
 LDAP = 'ldap.resel.fr'
-LDAP_ECOLE = '10.29.90.34'
-DN_ECOLE = "ou=people,o=unix,dc=enst-bretagne,dc=fr"
 DN_PEOPLE = 'ou=people,dc=maisel,dc=enst-bretagne,dc=fr'
 DN_MACHINES = 'ou=people,dc=maisel,dc=enst-bretagne,dc=fr'
+DN_ADMIN = 'cn=admin,dc=maisel,dc=enst-bretagne,dc=fr'
+PASSWD_ADMIN = 'kUpQusBpQtm60TFy'
