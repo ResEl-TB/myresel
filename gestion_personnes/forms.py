4070d1003f6d4a56def1706891163cd9b2a326f9
Morgan ROBIN <morgan@robin.wf>
2016-06-11 17:46:48 +0200

    Changement de la gestion du formulaire

--- gestion_personnes/forms.py
+++ gestion_personnes/forms.py
@@ -2,19 +2,16 @@ from django import forms
 from fonctions.ldap import search_ecole
 
 class InscriptionForm(forms.Form):
+    nom = forms.CharField()
+    prenom = forms.CharField()
+    mail = forms.EmailField()
+    mot_de_passe = forms.CharField(widget = forms.PasswordInput)
 
     def __init__(self, uid):
         super().__init__()
-        if len(search_ecole(uid)) == 0:
-            # Utilisateur non présent dans le LDAP école
-            self.nom = forms.CharField()
-            self.prenom = forms.CharField()
-            self.mail = forms.EmailField()
-        else:
-            # Utilisateur existant dans le LDAP école
+        if len(search_ecole(uid)) != 0:
+            # Utilisateur présent dans le LDAP école
             user = search_ecole(uid)[0]
             self.nom = forms.CharField(initial = user.cn[0].split(' ')[0])
             self.prenom = forms.CharField(initial = user.cn[0].split(' ')[1])
-            self.mail = forms.EmailField(initial = user.mail[0])
-
-    mot_de_passe = forms.CharField(widget = forms.PasswordInput)
+            self.mail = forms.EmailField(initial = user.mail[0])    
