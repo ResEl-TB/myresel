6f63ce819a1b8bc070affe7d917f7e3b75990140
Morgan ROBIN <morgan@robin.wf>
2016-06-12 17:04:29 +0200

    Ajout du formulaire de demande d'ajout de machine

--- gestion_machines/forms.py
+++ gestion_machines/forms.py
@@ -6,3 +6,6 @@ from django.utils.translation import ugettext_lazy as _
 class AjoutForm(forms.Form):
     alias = forms.CharField(label = _("Alias de la machine"), required = False)
     
+class AjoutManuelForm(forms.Form):
+    mac = forms.CharField(label = _("Adresse MAC de la machine"))
+    description = forms.CharField(label = _("Description de l'équipement à ajouter"), widget = forms.Textarea)
