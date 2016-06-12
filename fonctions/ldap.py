8598c314e7f9edafb4bb29360dd1cf7e19775ba2
Morgan ROBIN <morgan@robin.wf>
2016-06-12 12:50:25 +0200

    Modification de la fonction search et ajout de la fonction get_status

--- fonctions/ldap.py
+++ fonctions/ldap.py
@@ -1,16 +1,18 @@
 from ldap3 import Server, Connection
 from myresel.constantes import *
+import network
 
-def search(dn, query):
+def search(dn, query, attr = None):
     """ Fonction pour rechercher dans le ldap une entrée particulière 
         
         - dn : le DN dans lequel faire la recherche
         - query : la recherche en elle-même (uid, host, ip, mac, etc.)
+        - attr : les attributs à extraire de la fiche LDAP
     """
 
     res = False
     l = Connection(Server(LDAP, use_ssl = True), auto_bind = True)
-    if l.search(dn, query):
+    if l.search(dn, query, attributes = attr):
         res = l.entries
     l.unbind()
     return res
@@ -21,3 +23,33 @@ def add(dn, object_class, attributes):
     l = Connection(Server(LDAP, use_ssl = True), user = DN_ADMIN, password = PASSWD_ADMIN)
     l.add(dn, object_class, attributes)
     l.unbind()
+
+def get_status(ip):
+    """ Fonction pour trouver le status d'une machine :
+        - active
+        - inactive
+        - inexistante
+    """
+
+    # Identification du campus
+    campus = network.get_campus(ip)
+
+    # Récupération de l'adresse mac associée à l'IP
+    mac = network.get_mac(ip)
+
+    res = search(DN_MACHINES, '(&(macaddress=%s))' % mac, ['zone'])
+    if res:
+        if 'inactive' in [z.lower() for z in res[0].zone]:
+            # Machine inactive, on renvoit le status 'inactif'
+            return 'inactive'
+
+        elif campus.lower() not in [z.lower() for z in res[0].zone]:
+            # Machine sur le mauvais campus
+            return 'mauvais_campus'
+
+        else:
+            # Machine présente dans le LDAP, marquée comme active
+            return 'active'
+
+    # Machine inexistante dans le LDAP
+    return False
