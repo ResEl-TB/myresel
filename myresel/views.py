a11f32b1aab125eaec0d4b46c6f8d5dd63371337
Morgan ROBIN <morgan@robin.wf>
2016-06-12 16:34:31 +0200

    Ajout de la vue de News et modification de la vue Home

--- myresel/views.py
+++ myresel/views.py
@@ -8,6 +8,7 @@ from fonctions import ldap
 # Create your views here.
 class Home(TemplateView):
     """ Vue d'index, qui se charge de rediriger l'utilisateur sur la bonne page """
+
     template_name = 'myresel/home.html'
 
     def get(self, request, *args, **kwargs):
@@ -26,8 +27,18 @@ class Home(TemplateView):
                 return HttpRespondeRedirect(reverse('gestion_machines:changement-campus'))
 
             else:
-                # La machine est active, on affiche la page d'accueil traditionnelle
-                return render(request, self.template_name)
+                # La machine est active, on affiche la page de news
+                return HttpRespondeRedirect(reverse('news'))
 
-        # La machine n'existe pas dans le LDAP, on renvoit vers la page d'ajout de machine
-        return HttpRespondeRedirect(reverse('gestion_machines:ajout'))
+        """ La machine n'existe pas dans le LDAP, donc deux cas de figure :
+            - l'utilisateur est nouveau, dans ce cas il faut l'ajouter
+            - la machine est inconnue
+
+            La page affichée permet à l'utilisateur de choisir s'il doit s'inscrire lui ou sa machine
+        """
+        return render(request, self.template_name)
+
+class News(TemplateView):
+    """ Vue appelée pour afficher les news au niveau du ResEl """
+
+    template_name = 'myresel/news.html'
