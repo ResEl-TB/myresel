3e4f73155acd957e3bc990954f347fe94cbc25df
Morgan ROBIN <morgan@robin.wf>
2016-06-12 12:43:33 +0200

    Changement du DN des machines ResEl

--- myresel/constantes.py
+++ myresel/constantes.py
@@ -1,6 +1,6 @@
 # Constantes relatives au LDAP
 LDAP = 'ldap.resel.fr'
 DN_PEOPLE = 'ou=people,dc=maisel,dc=enst-bretagne,dc=fr'
-DN_MACHINES = 'ou=people,dc=maisel,dc=enst-bretagne,dc=fr'
+DN_MACHINES = 'ou=machines,dc=resel,dc=enst-bretagne,dc=fr'
 DN_ADMIN = 'cn=admin,dc=maisel,dc=enst-bretagne,dc=fr'
 PASSWD_ADMIN = 'kUpQusBpQtm60TFy'
