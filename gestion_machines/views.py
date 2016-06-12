e534f91010d830679bc406bf8b0107c9fcb63257
Morgan ROBIN <morgan@robin.wf>
2016-06-12 14:58:52 +0200

    Ecriture des vues réactivation et changement de campus

--- gestion_machines/views.py
+++ gestion_machines/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class Reactivation(TemplateView):
-	pass
+    template_name = 'gestion_machines/reactivation.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(Reactivation, self).dispatch(*args, **kwargs)
+
+	def get(self, request, *args, **kwargs):
+        ldap.reactivation(request.META['REMOTE_ADDR'])
+        return render(request, self.template_name)
 
 class Ajout(TemplateView):
 	pass
 
-class Campus(TemplateView):
-	pass
+class ChangementCampus(TemplateView):
+	template_name = 'gestion_machines/changement-campus.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ChangementCampus, self).dispatch(*args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        ldap.update_campus(request.META['REMOTE_ADDR'])
+        return render(request, self.template_name)
