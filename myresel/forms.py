0edbf8b255a0b445859926df712cf02f4802ff65
Morgan ROBIN <morgan@robin.wf>
2016-06-12 17:20:27 +0200

    Ajout du formulaire de contact

---
+++ myresel/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+# Pour la traduction - sert à marquer les chaînes de caractères à traduire
+from django.utils.translation import ugettext_lazy as _
+
+class ContactForm(forms.Form):
+    nom = forms.CharField(label = _("Votre nom"))
+    chambre = forms.CharField(label = _("Votre chambre"), initial = _("Bâtiment ET chambre"))
+    mail = forms.EmailField(label = _("Votre adresse mail"))
+    demande = forms.CharField(label = _("Votre demande"), widget = forms.Textarea)
