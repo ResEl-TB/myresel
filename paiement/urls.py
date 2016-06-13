ae0231bb18a8a2aa96ea52a6392019ceb399ac18
Morgan ROBIN <morgan@robin.wf>
2016-06-13 17:52:12 +0200

    Ajout des URLs vers la partie paiement

--- paiement/urls.py
+++ paiement/urls.py
@@ -1,4 +1,4 @@
-"""myresel URL Configuration
+"""paiement URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -13,17 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib.auth import views as auth_views
+from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
     url(r'^$', Home.as_view(), name = 'home'),
-    url(r'^login/', auth_views.login),
-    url(r'^logout/', auth_views.logout, {'next_page': '/'}),
-    url(r'^news$', News.as_view(), name = 'news'),
-    url(r'^machines/', include('gestion_machines.urls', namespace = 'gestion-machines')),
-    url(r'^personnes/', include('gestion_personnes.urls', namespace = 'gestion-personnes')),
-    url(r'^paiement/', include('paiement.urls', namespace = 'paiement')),
-    url(r'^contact$', Contact.as_view(), name = 'contact')
+    url(r'^historique$', Historique.as_view(), name = 'historique'),
 ]
