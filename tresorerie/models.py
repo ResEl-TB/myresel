7d39a5c5eb5e21cfbba45fd14e11ecfbc03e3108
Morgan ROBIN <morgan@robin.wf>
2016-06-14 01:20:16 +0200

    Ajout des max_length dans les CharField

--- tresorerie/models.py
+++ tresorerie/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class Produit(models.Model):
     """ Modèle pour la gestion des stocks sur les produits que l'on vend, ici une cotisation """
 
-    nom = models.CharField()
+    nom = models.CharField(max_length = 50)
     prix = models.FloatField()
 
 class Transaction(models.Model):
     """ Modèle pour la gestion des transactions réalisées """
 
-    utilisateur = models.CharField()
-    moyen = models.CharField(default = 'CB')
+    utilisateur = models.CharField(max_length = 50)
+    moyen = models.CharField(default = 'CB', max_length = 2)
     date = models.DateField(auto_now_add = True)
     produit = models.ManyToManyField(Produit)
     total = models.FloatField()
@@ -20,7 +20,7 @@ class Transaction(models.Model):
 class Mensualisation(models.Model):
     """ Modèle pour la gestion de la mensualisation """
 
-    utilisateur = models.CharField()
+    utilisateur = models.CharField(max_length = 50)
     nb_m = models.IntegerField() # Nombre de mensualités à payer
     nb_p = models.IntegerField(default = 0) # Nombre de mensualités payées
     customer = models.IntegerField() # ID de l'objet customer associé à l'utilisateur
