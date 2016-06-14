68d85d7c3549ad1b69245778f56b373b611d1454
Morgan ROBIN <morgan@robin.wf>
2016-06-14 15:55:33 +0200

    Ajout de classes css dans les input du formulaire

--- myresel/forms.py
+++ myresel/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 class ContactForm(forms.Form):
-    nom = forms.CharField(label = _("Votre nom"))
-    chambre = forms.CharField(label = _("Votre chambre"), widget = forms.TextInput(attrs = {'placeholder': _("Bâtiment ET chambre")}))
-    mail = forms.EmailField(label = _("Votre adresse mail"))
-    demande = forms.CharField(label = _("Votre demande"), widget = forms.Textarea)
+    nom = forms.CharField(label = _("Votre nom"), widget = forms.TextInput(attrs = {'class': 'form-control'}))
+    chambre = forms.CharField(label = _("Votre chambre"), widget = forms.TextInput(attrs = {'placeholder': _("Bâtiment ET chambre"), 'class': 'form-control'}))
+    mail = forms.EmailField(label = _("Votre adresse mail"), widget = forms.TextInput(attrs = {'class': 'form-control'}))
+    demande = forms.CharField(label = _("Votre demande"), widget = forms.Textarea(attrs = {'class': 'form-control', 'style': 'resize:none;'}))
