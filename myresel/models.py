58a1caa765533e6d22a65493091fafacfbc6edfb
Morgan ROBIN <morgan@robin.wf>
2016-06-14 13:49:07 +0200

    Ajout du modèle Billet pour gérer les news

---
+++ myresel/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+class Billet(models.Model):
+    titre = models.CharField(max_length = 100)
+    contenu = RichTextField()
