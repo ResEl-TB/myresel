588a15938c3b3db630f583259a8bfb194e618391
Morgan ROBIN <morgan@robin.wf>
2016-06-14 13:55:13 +0200

    Retrait les Users et des Groups de la page admin

--- myresel/admin.py
+++ myresel/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 from .models import *
 
+admin.site.unregister(User)
+admin.site.unregister(Group)
 admin.site.register(Billet)
