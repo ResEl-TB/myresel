2e7430563ee47e8e59df0743a134507efd110556
Morgan ROBIN <morgan@robin.wf>
2016-06-14 23:47:17 +0200

    Rename inscription --> myresel

--- myresel/wsgi.py
+++ myresel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for inscription project.
+WSGI config for myresel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inscription.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myresel.settings")
 
 application = get_wsgi_application()
