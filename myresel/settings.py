00e724bc0d2669f5d7fbca118fe95a0a0ca19a27
Morgan ROBIN <morgan@robin.wf>
2016-06-14 13:22:13 +0200

    Ajout du template de base, et des fichiers statiques de base

--- myresel/settings.py
+++ myresel/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'myresel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
