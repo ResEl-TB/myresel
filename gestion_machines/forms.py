8e55e25db9cc4726682a28935b4586d0bca6a48f
Morgan ROBIN <morgan@robin.wf>
2016-06-14 20:23:12 +0200

    Clean de l'alias dans la validation du formulaire

--- gestion_machines/forms.py
+++ gestion_machines/forms.py
@@ -1,10 +1,28 @@
 from django import forms
+from myresel.constantes import DN_MACHINES
+from fonctions import ldap
+
+import re
 
 # Pour la traduction - sert à marquer les chaînes de caractères à traduire
 from django.utils.translation import ugettext_lazy as _
 
 class AjoutForm(forms.Form):
     alias = forms.CharField(label = _("Alias de la machine"), required = False, widget = forms.TextInput(attrs = {'class': 'form-control'}))
+
+    def clean_alias(self):
+        alias = self.cleaned_data['alias']
+
+        if len(alias) < 5:
+            raise forms.ValidationError(_("Longueur d'alias trop courte"), code = 'invalid')
+
+        if not re.match(r'^[a-z0-9-]{5,}$', alias):
+            raise forms.ValidationError(_("Alias non conforme"), code = 'invalid')
+
+        if not ldap.search(DN_MACHINES, '(|(host=%(alias)s)(hostalias=%(alias)s))' % {'alias': alias}):
+            raise forms.ValidationError(_("Alias non disponible"), code = 'invalid')
+
+        return alias
     
 class AjoutManuelForm(forms.Form):
     mac = forms.CharField(label = _("Adresse MAC de la machine"), widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'xx:xx:xx:xx:xx:xx'}))
