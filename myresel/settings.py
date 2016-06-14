e6d072b8e556d32089ee4f07d3100a35aa690433
Morgan ROBIN <morgan@robin.wf>
2016-06-14 13:49:34 +0200

    Ajout du module ckeditor pour django

--- myresel/settings.py
+++ myresel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'gestion_machines',
     'gestion_personnes',
     'myresel',
