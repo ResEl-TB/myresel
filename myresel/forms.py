3c5882fc3946be08e87bbbfb77fdabb07deede54
Morgan ROBIN <morgan@robin.wf>
2016-06-14 15:35:29 +0200

    Ajout d'élément dans le placeholder du formulaire de contact

--- myresel/forms.py
+++ myresel/forms.py
@@ -5,6 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class ContactForm(forms.Form):
     nom = forms.CharField(label = _("Votre nom"))
-    chambre = forms.CharField(label = _("Votre chambre"), initial = _("Bâtiment ET chambre"))
+    chambre = forms.CharField(label = _("Votre chambre"), widget = forms.TextInput(attrs = {'placeholder': _("Bâtiment ET chambre")}))
     mail = forms.EmailField(label = _("Votre adresse mail"))
     demande = forms.CharField(label = _("Votre demande"), widget = forms.Textarea)
