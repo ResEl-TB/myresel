d3e26e83438ab7da6c4f0bd4501034759499d244
Dimtion <zizou.xena@gmail.com>
2016-06-21 10:17:40 +0200

    Repair potential bug in network configuration

--- fonctions/network.py
+++ fonctions/network.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 
+
 def get_mac(ip):
     """ Fonction qui récupère l'addresse MAC associée à l'IP de l'utilisateur """
 
@@ -9,25 +10,25 @@ def get_mac(ip):
                             shell=True).communicate()[0]).split('\'')[1].split('\\n')[0]
     return mac
 
+
 def get_campus(ip):
     """ Détermine le campus en fonction de l'ip de l'utilisateur """
 
-    if '172.22' in ip:
+    if ip.startswith('172.22.'):
         return 'Brest'
-    elif '172.23' in ip:
+    elif ip.startswith('172.23.'):
         return 'Rennes'
     else:
         return False
+
 
 def update_all():
     """ Relance le DNS, le DHCP et le firewall """
     os.system('ssh -t reloader@saymyname')
     os.system('ssh -t updatefirewall@zahia -p 2222')
 
+
 def is_resel_ip(ip):
     """ Check si l'IP est ResEl """
 
-    if '172.22' in ip or '172.23' in ip:
-        return True
-
-    return False
+    return True if get_campus(ip) else False
