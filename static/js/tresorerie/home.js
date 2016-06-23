07c4c1d9775a7f98280331732ba1ccccfd054068
Morgan ROBIN <morgan@robin.wf>
2016-06-24 00:18:25 +0200

    Template + formulaire fonctionnels

--- static/js/tresorerie/home.js
+++ static/js/tresorerie/home.js
@@ -1,79 +1,93 @@
-var trees = [];
+var trees;
+
+function main(formation, member) {
+    var tree;
+    trees = [];
+
+    $('.choices').removeClass('hidden');
+    $('.stripe').addClass('hidden');
+
+    if (member === 'false') {
+        if (!formation.toLowerCase().includes('fip')) {
+            tree = [
+                {'name': 'choice', 'question': gettext("Que souhaitez-vous payer ?"), 'children': [
+                    {'option': gettext("Adhésion seule - 1 €"), 'price': '100'},
+
+                    {'name': 'duration', 'option': gettext("Pack tout compris (adhésion + accès Internet)"), 'question': gettext("Combien de temps restez-vous sur le campus ?"), 'children': [
+                        {'option': gettext("1 mois - 11 €"), 'price': '1100'},
+
+                        {'name': '3x6m', 'option': gettext("6 mois - 51 €"), 'question': gettext("Souhaitez-vous payer en 3 fois ?"), 'children': [
+                            {'option': gettext("Oui - 17,70 € puis 16,70 € par mois"), 'price': '1770'},
+
+                            {'option': gettext("Non - 51 €"), 'price': '5100'}
+                        ]},
+
+                        {'name': '3x1a', 'option': gettext("1 an - 86 €"), 'question': gettext("Souhaitez-vous payer en 3 fois ?"), 'children': [
+                            {'option': gettext("Oui - 29,40 € puis 28,40 €"), 'price': '2940'},
+
+                            {'option': gettext("Non - 86 €"), 'price': '8600'}
+                        ]}
+                    ]}
+                ]}
+            ]; 
+        } else {
+            tree = [
+                {'name': 'choice', 'question': gettext("Que souhaitez-vous payer ?"), 'children': [
+                    {'option': gettext("Adhésion seule - 1 €"), 'price': '100'},
+
+                    {'name': '3x6m', 'option': gettext("Pack tout compris (adhésion + accès Internet) - 51 €"), 'question': gettext("Souhaitez-vous payer en 3 fois ?"), 'children': [
+                        {'option': gettext("Oui - 17,70 € puis 16,70 € par mois"), 'price': '1770'},
+
+                        {'option': gettext("Non - 51 €"), 'price': '5100'} 
+                    ]}
+                ]}
+            ];
+        };  
+    } else {
+        if (!formation.toLowerCase().includes('fip')) {
+            tree = [
+                {'name': 'choice', 'question': gettext("Combien de temps restez-vous sur le campus ?"), 'children': [
+                    {'option': gettext("1 mois - 11 €"), 'price': '1100'},
+
+                    {'name': '3x6m', 'option': gettext("6 mois - 51 €"), 'question': gettext("Souhaitez-vous payer en 3 fois ?"), 'children': [
+                        {'option': gettext("Oui - 17,70 € puis 16,70 € par mois"), 'price': '1770'},
+
+                        {'option': gettext("Non - 51 €"), 'price': '5100'}
+                    ]},
+
+                    {'name': '3x1a', 'option': gettext("1 an - 86 €"), 'question': gettext("Souhaitez-vous payer en 3 fois ?"), 'children': [
+                        {'option': gettext("Oui - 29,40 € puis 28,40 €"), 'price': '2940'},
+
+                        {'option': gettext("Non - 86 €"), 'price': '8600'}
+                    ]}
+                ]}
+            ];
+        } else {
+            tree = [
+                {'name': 'choice', 'question': gettext("Souhaitez-vous payer en 3 fois ?"), 'children': [
+                    {'option': gettext("Oui - 17,70 € puis 16,70 € par mois"), 'price': '1770'},
+
+                    {'option': gettext("Non - 51 €"), 'price': '5100'} 
+                ]}
+            ];
+        };
+    };
+
+    show(tree[0]);
+}
 
 function show(tree) {
-    $('#pricing').html(
+    $('.choices').html(
             '<h3>' + tree['question'] + '</h3>'
     );
 
     $.each(tree['children'], function(index, child) {
-        $('#pricing').append(
+        $('.choices').append(
             '<label class="radio-inline">' + 
-                '<input class="selector" type="radio" name=' + tree['option'] + ' index=' + index + '>' + child['option'] + 
+                '<input class="selector" type="radio" name=' + tree['name'] + ' index=' + index + '>' + child['option'] + 
             '</label>'
         );
         trees.push(child);
-        $('input[name=' + tree['option'] + '][index=' + index + ']').attr('tree', Object.keys(trees).length - 1);
+        $('input[name=' + tree['name'] + '][index=' + index + ']').attr('tree', Object.keys(trees).length - 1);
     });
 };
-
-$(function () {
-    const OPTIONS = [
-        {'option': 'first', 'question': gettext('Quelle est votre formation ?'), 'children': [
-            {'option': 'Autre', 'price': '1100'},
-
-            {'option': 'FIP', 'question': gettext('Voulez-vous payer en plusieurs fois ?'), 'children': [
-                {'option': 'Oui', 'price': '1767'},
-                {'option': 'Non', 'price': '5100'}
-            ]},
-
-            {'option': 'FIG', 'question': gettext('En quelle année êtes-vous ?'), 'children': [
-                {'option': '1A', 'question': gettext('Souhaitez-vous payer en 3 fois ?'), 'children': [
-                    {'option': 'Oui', 'price': '2940'},
-                    {'option': 'Non', 'price': '8600'}
-                ]},
-
-                {'option': '2A', 'question': gettext('Combien de temps restez-vous sur le campus ?'), 'children': [
-                    {'option': '6 mois', 'question': gettext('Souhaitez-vous payer en 3 fois ?'), 'children': [
-                        {'option': 'Oui', 'price': '1767'},
-                        {'option': 'Non', 'price': '5100'}
-                    ]},
-
-                    {'option': '1 an', 'question': gettext('Souhaitez-vous payer en 3 fois ?'), 'children': [
-                        {'option': 'Oui', 'price': '2940'},
-                        {'option': 'Non', 'price': '8600'}
-                    ]}
-                ]},
-
-                {'option': '3A', 'question': gettext('Souhaitez-vous payer en 3 fois ?'), 'children': [
-                    {'option': 'Oui', 'price': '1767'},
-                    {'option': 'Non', 'price': '5100'}
-                ]}
-            ]}
-        ]}
-    ];
-
-    show(OPTIONS[0]);
-
-    $('#pricing').on('click', '.selector', function () {
-        var tree = trees[parseInt($(this).attr('tree'))];
-
-        if ('children' in tree) {
-            show(tree);
-        } else {
-            $('#stripe').html(
-                '<br> \
-                <script \
-                    src="https://checkout.stripe.com/checkout.js" class="stripe-button" \
-                    data-key="pk_test_KE9qQquz3hhdRI54CcfBaukl" \
-                    data-amount="' + tree['price'] + '" \
-                    data-name="Association ResEl" \
-                    data-description="Widget" \
-                    data-image="/static/images/tresorerie/marketplace.png" \
-                    data-locale="auto" \
-                    data-zip-code="true" \
-                    data-currency="eur"> \
-                </script>'
-            );
-        }
-    });
-});
