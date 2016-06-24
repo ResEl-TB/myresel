c8c17e8172b67f7eb2a93f157e26748c2d04de78
Jacquin Théo <42nimag@gmail.com>
2016-06-24 17:39:28 +0200

    [custom-pages] Ajout nb articles max dans navbar
    Nombres de pages affichées limités dans le menu déroulant pour des
    raisons d'UI.

--- wiki/context_processors.py
+++ wiki/context_processors.py
@@ -1,7 +1,12 @@
 from django.conf import settings
 from .models import Category
+from myresel.constantes import NB_ARTICLES_TO_DISPLAY_IN_SUBMENU
 
 def articles_in_menu(request):
+    """Fonction pour générer les catégories et leurs articles dans la navbar
+
+    Appellée automatiquement en tant que context processor
+    """
     categories = Category.objects.all().order_by('-id')
-    return {'categories': categories}
+    return {'categories': categories[:NB_ARTICLES_TO_DISPLAY_IN_SUBMENU], 'nbCategories': len(categories)}
 
