85d80d7d11201e03798a4da244846e3d8b015be4
Morgan ROBIN <morgan@robin.wf>
2016-06-24 15:31:22 +0200

    Correct verbose_name

--- custom_pages/apps.py
+++ custom_pages/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CustomPagesConfig(AppConfig):
     name = 'custom_pages'
-    verbose_name = 'Pages personnalisés'
+    verbose_name = 'Pages personnalisées'
