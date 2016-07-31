a7359c5611003500d943b65e76dd036b1c4ac170
www-data <www-data@skynet.pub.maisel.enst-bretagne.fr>
2016-07-31 15:20:07 +0200

    Fix mac recuperation + improve time execution

--- fonctions/network.py
+++ fonctions/network.py
@@ -5,15 +5,16 @@ import re
 def get_mac(ip):
     """ Fonction qui récupère l'addresse MAC associée à l'IP de l'utilisateur """
 
-    if re.match(r'172\.22\.22[4-5]', ip):
+    if re.match(r'^172\.22\.22[4-5]', ip):
         eth = 'eth3'
     else:
         eth = 'eth2'
-    mac = str(subprocess.Popen(["sudo arping -c 1 -i {} -a {} | awk 'NR == 2 {{print $4}}'".format(eth, ip)], 
+   
+    subprocess.check_call(['ping', '-c', '1', '-I', eth, ip], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) 
+    mac = str(subprocess.Popen(["arp -a | grep {} | awk '{{print $4}}'".format(ip)], 
                             stdout = subprocess.PIPE, 
                             shell=True).communicate()[0]).split('\'')[1].split('\\n')[0]
     return mac
-
 
 def get_campus(ip):
     """ Détermine le campus en fonction de l'ip de l'utilisateur """
