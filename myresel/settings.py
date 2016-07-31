98510f9a37e3bd54bc3aaa8f0b9c775245df57e0
www-data <www-data@skynet.pub.maisel.enst-bretagne.fr>
2016-07-31 12:29:30 +0200

    Pre-merge settings

--- myresel/settings.py
+++ myresel/settings.py
@@ -47,7 +47,7 @@ LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
 
 AUTHENTICATION_BACKENDS = (
-    #'django_python3_ldap.auth.LDAPBackend',
+    'django_python3_ldap.auth.LDAPBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
