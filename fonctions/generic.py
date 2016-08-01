34fdc4279a5cc782cfa41452ea1ff84ccb2df6b9
Théo JACQUIN <theo.jacquin@telecom-bretagne.eu>
2016-08-01 02:34:39 -0300

    Add unit functions
    Added two functions :
    - is_ip_in_subnet in generic.py that check if ip is in subnet : subnet_ip/netmask
    - get_zone in network.py that return the zone belonging to an ip address

--- fonctions/generic.py
+++ fonctions/generic.py
@@ -32,3 +32,23 @@ def hash_to_ntpass(password):
     """ Fourni un hash du mdp pour correspondre avec le loggin over Wi-Fi """
 
     return str(binascii.hexlify(hashlib.new('md4', password.encode('utf-16le')).digest()).upper()).split('\'')[1]
+
+def ip_to_int(ip):
+    """ Convert an IP in int"""
+    o = map(int, ip.split('.'))
+    res = (16777216 * o[0]) + (65536 * o[1]) + (256 * o[2]) + o[3]
+    return res
+
+def is_ip_in_subnet(ip, ipNetwork, maskLength):
+    """ Check if ip is in the subnet ipNetwork/maskLength"""
+    ipInt = ip_to_int(ip)
+    maskLengthFromRight = 32 - maskLength
+    ipNetworkInt = ip_to_int(ipNetwork) #convert the ip network into integer form
+    binString = "{0:b}".format(ipNetworkInt) #convert that into into binary (string format)
+    chopAmount = 0 #find out how much of that int I need to cut off
+    for i in range(maskLengthFromRight):
+        if i < len(binString):
+            chopAmount += int(binString[len(binString)-1-i]) * 2**i
+    minVal = ipNetworkInt-chopAmount
+    maxVal = minVal+2**maskLengthFromRight -1
+    return minVal <= ipInt and ipInt <= maxVal
--- fonctions/network.py
+++ fonctions/network.py
@@ -2,6 +2,8 @@ import subprocess
 import os
 import re
 
+from fonction.generic import is_ip_in_subnet
+
 def get_mac(ip):
     """ Fonction qui récupère l'addresse MAC associée à l'IP de l'utilisateur """
 
@@ -37,3 +39,24 @@ def is_resel_ip(ip):
     """ Check si l'IP est ResEl """
 
     return True if get_campus(ip) else False
+
+def get_network_zone(ip):
+    """ 
+    Renvoit la zone à laquelle correspond l'ip :
+    - Brest-user, Brest-inscription-999, Brest-inscription, Rennes-user, Rennes-inscription
+    """
+
+    if is_ip_in_subnet(ip, '172.22.224.0', 23):
+        return "Brest-inscription"
+    else if is_ip_in_subnet(ip, '172.22.226.0', 23):
+        return "Brest-inscription-999"
+    else if ip.startswith(172.22.) and ip[7:10].isdigit() and 200 <= int(ip[7:10]) <= 223: # range 172.22.200.1 to 172.22.223.254 
+        return "Brest-user"
+    else if ip.startswith('172.22.'):
+        return "Brest-other"
+    else if is_ip_in_subnet(ip, '172.23.224.0', 23):
+        return "Rennes-inscription"
+    else if ip.startswith('172.23'): # TODO : check the full pattern
+        return "Rennes-user"
+
+    return "Internet"
