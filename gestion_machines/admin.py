d8c2c1fff5d0ae905acdcdc3fc767b4b42335378
Dimtion <zizou.xena@gmail.com>
2016-08-03 23:52:54 +0200

    Add admin for devices and users

--- gestion_machines/admin.py
+++ gestion_machines/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 
-# Register your models here.
+from gestion_machines.models import LdapDevice
+
+
+class LdapDeviceAdmin(TranslationAdmin):
+    list_display = (
+        'uid',
+        'displayname',
+        'promo',
+        'state',
+        'mail',
+        'batiment',
+    )
+    list_filter = ['promo', 'cotiz', 'batiment']
+
+admin.site.register(LdapDevice, LdapDeviceAdmin)
--- gestion_personnes/admin.py
+++ gestion_personnes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 
-# Register your models here.
+from gestion_personnes.models import LdapUser
+
+
+class LdapUserAdmin(TranslationAdmin):
+    list_display = (
+        'hostname',
+        'ip',
+        'uid',
+        'zone',
+    )
+    list_filter = ['zone']
+
+admin.site.register(LdapUser, LdapUserAdmin)
