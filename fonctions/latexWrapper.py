003231fea5eba5156594ccb97d3d29413d82b2b1
Dimtion <zizou.xena@gmail.com>
2016-08-03 09:32:18 +0200

    Function formating  - Add `def` at the `generate_pdf` function declaration  - Correct variable typo  - Change tabs into spaces  - Add new line at the end of the file
     :)

--- fonctions/latexWrapper.py
+++ fonctions/latexWrapper.py
@@ -5,25 +5,26 @@ from tempfile import mkdtemp, mkstemp
 
 from django.template.loader import render_to_string
 
-generate_pdf(template_path, template_variable, dest_name, dest_folder):
-	# Get some temp files
-	tmp_folder = mkdtemp()
-	os.chdir(tmp_folder)
-	texfile, texfilename = mkstemp(dir=tmp_folder)
 
-	# Render to template with var
-	os.write(texfile, render_to_string(template_path, template_variables))
-	os.close(texfile)
+def generate_pdf(template_path, template_variable, dest_name, dest_folder):
+    # Get some temp files
+    tmp_folder = mkdtemp()
+    os.chdir(tmp_folder)
+    texfile, texfilename = mkstemp(dir=tmp_folder)
 
-	# Compile the TeX file with PDFLaTeX
-	call(['pdflatex', texfilename])
+    # Render to template with var
+    os.write(texfile, render_to_string(template_path, template_variable))
+    os.close(texfile)
 
-	# Make pdf permanent
-	os.rename(texfilename + '.pdf', os.path.join(dest_folder, dest_name + '.pdf'))
+    # Compile the TeX file with PDFLaTeX
+    call(['pdflatex', texfilename])
 
-	# Cleanup
-	os.remove(texfilename)
-	os.remove(texfilename + '.aux')
-	os.remove(texfilename + '.log')
-	os.rmdir(tmp_folder)
-	return os.path.join(dest_folder, dest_name + '.pdf')
+    # Make pdf permanent
+    os.rename(texfilename + '.pdf', os.path.join(dest_folder, dest_name + '.pdf'))
+
+    # Cleanup
+    os.remove(texfilename)
+    os.remove(texfilename + '.aux')
+    os.remove(texfilename + '.log')
+    os.rmdir(tmp_folder)
+    return os.path.join(dest_folder, dest_name + '.pdf')
