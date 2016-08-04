54327f0a7b55bf6d52cb08427fd43879e00c0756
Théo Jacquin <theo.jacquin@telecom-bretagne.eu>
2016-08-04 02:38:17 +0200

    Solve several problems
    - Solve conflict between latex and django templating system :
    Problem with {{{ misunderstood by django template, now use {% templatetag openbrace %}
    - Solve probleme of os.move in python :
    os.move doesn't work with src and dst on different unix partition

--- fonctions/latexWrapper.py
+++ fonctions/latexWrapper.py
@@ -2,11 +2,16 @@ import os
 
 from subprocess import call
 from tempfile import mkdtemp, mkstemp
+from shutil import copyfile
 
 from django.template.loader import render_to_string
 
 
 def generate_pdf(template_path, template_variable, dest_name, dest_folder):
+	"""
+	Ex :
+	generate_pdf('tresorerie/facture.tex', {'confLang': 'fr', 'user': {'uid': 'tjacquin', 'firstName':'Théo', 'lastName': 'Jacquin', 'addressFirstPart': '3 rue Coquelicot', 'addressSecondPart': '97345 Guyane'}, 'invoice': {'id': 1234567, 'date':'\\today', 'internetFeesPrice': '84.5', 'isPaid':'yes', 'payment': {'date':'\\today', 'info': 'Carte Bleue, virement n 3456765432'}}}, 'test_facture', '/srv/www/resel.fr/media/invoices')
+	"""
     # Get some temp files
     tmp_folder = mkdtemp()
     os.chdir(tmp_folder)
@@ -17,13 +22,14 @@ def generate_pdf(template_path, template_variable, dest_name, dest_folder):
     os.close(texfile)
 
     # Compile the TeX file with PDFLaTeX
-    call(['pdflatex', texfilename])
+    call(['pdflatex', '-interaction=batchmode', texfilename])
 
     # Make pdf permanent
-    os.rename(texfilename + '.pdf', os.path.join(dest_folder, dest_name + '.pdf'))
+    copyfile(texfilename + '.pdf', os.path.join(dest_folder, dest_name + '.pdf'))
 
     # Cleanup
     os.remove(texfilename)
+    os.remove(texfilename + '.pdf')
     os.remove(texfilename + '.aux')
     os.remove(texfilename + '.log')
     os.rmdir(tmp_folder)
