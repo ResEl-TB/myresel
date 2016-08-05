dc8106d33c2cfc18edac2973691f5d8e74d5f074
Théo Jacquin <theo.jacquin@telecom-bretagne.eu>
2016-08-05 02:44:27 +0200

    Add some info to email sent at inscription

--- gestion_personnes/views.py
+++ gestion_personnes/views.py
@@ -105,8 +105,20 @@ class InscriptionCGU(View):
                 "\nVous êtes désormais inscrit au ResEl, voici vos identifiants :" +
                 "\nNom d'utilisateur : " + str(user.uid) +
                 "\nMot de passe : **** (celui que vous avez choisi lors de l'inscription)" +
+                "\n\n Vous pouvez, si vous souhaitez, changer votre mot de passe (en suivant ce lien https://my.resel.fr/personnes/modification-passwd)" +
+                "\n Ainsi que tout les paramètres de votre compte." +
                 "\n\n En étant membre de l'association ResEl vous pouvez profiter de ses nombreux services et des "
-                  "activités que l'association propose.",
+                  "activités que l'association propose." +
+                "\n N'hésitez pas à naviguer sur notre site (https://resel.fr) pour y découvrir tout ce que nous proposons." +
+                "\n\nPour avoir accès à internet, vous allez devoir inscrire chacune de vos machines (ordinateurs, smartphones, etc...) à notre réseau." +
+                "\nRendez vous sur notre site web, vous serez guidé à travers cette dernière étape." +
+                "\n\nSi vous avez le moindre problème, la moindre question, la moindre envie de nous féliciter, ou de nous faire des bisous baveux,"+
+                "vous pouvez répondre à cet e-mail, ou venir nous voir pendant nos permanences, celles-ci ont lieu tous les jours en semaine de 18h à 19h30"+
+                "au foyer des élèves de Télécom Bretagne."+
+                "\n\nSi vous êtes intéressé pour nous aider, pour travailler avec nous au sein de l'association, pour mettre à disposition vos compétences,"+
+                "ou même si vous n'avez pas de compétences mais que vous souhaitez apprendre, vous pouvez aussi nous contacter pour faire partie de l'équipe" +
+                " d'administrateurs !"+
+                "\n\nÀ bientôt, l'équipe ResEl.",
                 from_email=SERVER_EMAIL,
                 reply_to=["support@resel.fr"],
                 to=[user.mail],
