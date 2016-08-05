ef9ad14589ceab75c556845f17be2061507a7858
Dimtion <zizou.xena@gmail.com>
2016-08-05 16:47:17 +0200

    Continue vagrant integration

--- myresel/credentials.py
+++ myresel/credentials.py
@@ -4,18 +4,34 @@ Credentials for the ResEl website
 
 # LDAP
 
-LDAP_URL = "ldap.resel.fr"
+LDAP_URL = "localhost"
 LDAP_PORT = "389"
-LDAP_PASSWD = 'BLAH'
+LDAP_PASSWD = 'blah'
 
 LDAP_DC = "dc=maisel,dc=enst-bretagne,dc=fr"
 LDAP_DN_PEOPLE = 'ou=people,%s' % LDAP_DC
 LDAP_DN_MACHINES = 'ou=machines,%s' % LDAP_DC
 LDAP_DN_ADMIN = 'cn=admin,%s' % LDAP_DC
 
+LDAP_ECOLE = "10.29.90.34"
+LDAP_ECOLE_DN = "ou=people,o=unix,dc=enst-bretagne,dc=fr"
 
-# ADMIN DATABASE
 
-DB_ADMIN_HOST = "maia.adm.resel.fr"
-DB_ADMIN_USER = "myresel"
-DB_ADMIN_PASSWORD = "BLAH"
+# DATABASE
+
+DB_NAME = "resel"
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "blah"
+
+
+# REDIS
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+
+# STRIPE
+
+STRIPE_API_KEY = "sk_test_Uk3Qcg8o0OTj8VHAG6NovqR9"
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,38 +4,48 @@
 
 # Configuration
 ROOTDIR=/myresel/
-ETCDIR=vagrant/etc/
+ETCDIR=${ROOTDIR}vagrant/etc/
+LIBDIR=${ROOTDIR}vagrant/lib/
 
 # TODO: move that to python config
 LDAP_PASSWD=blah
-LDAP_DOMAIN="maisel.enst-bretagne.fr"
-LDAP_ORGANISATION="ou=people,dc=maisel,dc=enst-bretagne,dc=fr"
+#LDAP_DOMAIN="maisel.enst-bretagne.fr"
+#LDAP_ORGANISATION="ou=people,dc=maisel,dc=enst-bretagne,dc=fr"
 
-SQLHOST=localhost
-SQLNAME=resel
-SQLUSER=resel
-SQLPASSWD=blah
+SQL_HOST=localhost
+SQL_DBNAME=resel
+SQL_USER=resel
+SQL_PASSWD=blah
 
 export DEBIAN_FRONTEND=noninteractive
 
-echo 'apt-get update'
+echo '>>> apt-get update&upgrade'
 apt-get -qq update
+apt-get -qq upgrade
 
-echo 'Installing : build-essential python-software-properties python3 python3-dev python3-pip'
+echo '>>> Installing : build-essential python-software-properties python3 python3-dev python3-pip'
 apt-get -qq install build-essential python-software-properties python3 python3-dev python3-pip vim
 
-echo "Installing mysql"
-echo "mysql-server mysql-server/root_password password $SQLPASSWD" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password $SQLPASSWD" | debconf-set-selections
+echo ">>> Installing mysql"
+echo "mysql-server mysql-server/root_password password $SQL_PASSWD" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $SQL_PASSWD" | debconf-set-selections
 apt-get -qq install mysql-server
 apt-get -qq install libmysqlclient-dev
 
-echo "configuring slapd for first run"
+echo -e ">>> Setting up MySQL"
+mysql -uroot -p${SQL_PASSWD} -e "CREATE DATABASE ${SQL_DBNAME}"
+#mysql -uroot -p${SQL_PASSWD} -e "CREATE USER 'root'@'localhost' IDENTIFIED BY '$SQL_PASSWD';"
+#mysql -uroot -p${SQL_PASSWD} -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';"
+mysql -uroot -p${SQL_PASSWD} -e "CREATE USER '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PASSWD';"
+mysql -uroot -p${SQL_PASSWD} -e "GRANT ALL PRIVILEGES ON * . * TO '$SQL_USER'@'localhost';"
+
+echo ">>> Installing openldap"
 
 
 apt-get -qq install slapd ldap-utils libldap2-dev libsasl2-dev libssl-dev ldapvi
+service slapd stop
 
-cd $ROOTDIR
+cd ${ROOTDIR}
 cp -rf "${ETCDIR}ldap/" /etc/
 
 PASSWD_HASH=$(slappasswd -h {SSHA} -s ${LDAP_PASSWD})
@@ -50,13 +60,21 @@ chmod +r /etc/ldap/rootdn.ldap
 mkdir /var/lib/ldap/maisel
 mkdir /var/lib/ldap/resel
 
-service slapd stop
+cp ${LIBDIR}DB_CONFIG_maisel /var/lib/ldap/maisel/DB_CONFIG
+cp ${LIBDIR}DB_CONFIG_resel /var/lib/ldap/resel/DB_CONFIG
+
+# TODO: empty (a bit) the database
+slapadd -c -l ${LIBDIR}maisel.ldif -b dc=maisel,dc=enst-bretagne,dc=fr
+slapadd -c -l ${LIBDIR}resel.ldif -b dc=resel,dc=enst-bretagne,dc=fr
+
+chown -R openldap:openldap /var/lib/ldap/
+
 service slapd start
 
-#
-#echo "Installing other dependecies"
-#apt-get -y install libjpeg-dev
-##apt-get install libssl-dev
-#
-#echo "Installing Python requirements"
-#pip3 install -r $ROOTDIR/requirements.txt
+
+echo ">>> Installing other dependecies"
+apt-get -qq install libjpeg-dev
+apt-get -qq install libssl-dev
+
+echo ">>> Installing Python requirements"
+pip3 install -r $ROOTDIR/requirements.txt
