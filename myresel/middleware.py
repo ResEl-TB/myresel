ce87376c939ff11365ae7a918d77360e2415bfbe
Théo Jacquin <theo.jacquin@telecom-bretagne.eu>
2016-08-06 22:57:28 +0200

    Prevent unsubscribed user to browse to the website

--- myresel/middleware.py
+++ myresel/middleware.py
@@ -110,12 +110,12 @@ class inscriptionNetworkHandler(object):
                 # Check if logged in & registered:
                 if is_registered == 'active' and is_logged_in:
                     messages.warning(request, _("Vous êtes correctement inscrit, mais vous êtes sur le mauvais réseau. Veuillez vous connecter sur le réseau Wifi 'ResEl Secure'"))
-                # else:
-                #     # We check that he only browses intended part of the website
-                #     path = request.path_info.lstrip('/')
-                #     if not any(m.match(path) for m in INSCRIPTION_ZONE_ALLOWED_URLS):
-                #         messages.info(request, _("Vous devez vous inscrire au ResEl avant de pouvoir naviguer normalement."))
-                #         return HttpResponseRedirect(settings.LOGIN_URL)
+                else:
+                    # We check that he only browses intended part of the website
+                    path = request.path_info.lstrip('/')
+                    if not any(m.match(path) for m in INSCRIPTION_ZONE_ALLOWED_URLS):
+                        messages.info(request, _("Vous devez vous inscrire au ResEl avant de pouvoir naviguer normalement."))
+                        return HttpResponseRedirect(settings.LOGIN_URL)
 
         elif vlan == '999':
 
@@ -131,12 +131,12 @@ class inscriptionNetworkHandler(object):
                 # Check if logged in & registered:
                 if is_registered == 'active' and is_logged_in:
                     messages.warning(request, _("Votre inscription n'est pas finie. Veuillez vous déconnecter puis vous reconnecter sur le réseau Wifi 'ResEl Secure'"))
-                    # else:
-                    #     # We check that he only browses intended part of the website
-                    #     path = request.path_info.lstrip('/')
-                    #     if not any(m.match(path) for m in INSCRIPTION_ZONE_ALLOWED_URLS):
-                    #         messages.info(request, _("Vous devez vous inscrire au ResEl avant de pouvoir naviguer normalement."))
-                    #         return HttpResponseRedirect(settings.LOGIN_URL)
+                    else:
+                        # We check that he only browses intended part of the website
+                        path = request.path_info.lstrip('/')
+                        if not any(m.match(path) for m in INSCRIPTION_ZONE_ALLOWED_URLS):
+                            messages.info(request, _("Vous devez vous inscrire au ResEl avant de pouvoir naviguer normalement."))
+                            return HttpResponseRedirect(settings.LOGIN_URL)
 
             else:
                 # Other possiblities: Brest-inscription, Brest-other.
