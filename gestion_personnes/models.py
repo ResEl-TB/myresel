8ebf94b3ec770de677c9b93ad8b726803206c4fd
Dimtion <zizou.xena@gmail.com>
2016-08-08 17:38:57 +0200

    Create Model LUser which implements Ldapback

--- gestion_personnes/models.py
+++ gestion_personnes/models.py
@@ -1,14 +1,77 @@
 # coding=utf-8
 import json
 
+import ldapback
 import ldapdb
 from django.db import models
 
 # DO NOT INHERIT MODELS, it won't work on the ldap
 
 # Create your models here.
+from ldapback.models.fields import LdapCharField, LdapPasswordField, LdapNtPasswordField, LdapListField
 from myresel.settings import LDAP_DN_PEOPLE
 from ldapdb.models.fields import CharField, IntegerField, ListField
+
+
+# TODO: change class name
+class LUser(ldapback.models.LdapModel):
+    """
+    The class having all the element for a user
+    """
+    base_dn = LDAP_DN_PEOPLE
+    object_classes = ['genericPerson', 'enstbPerson', 'reselPerson', 'maiselPerson', 'aePerson']
+
+    # genericPerson
+    uid = LdapCharField(db_column='uid', object_classes=['genericPerson'], pk=True)
+    first_name = LdapCharField(db_column='firstname', object_classes=['genericPerson'])
+    last_name = LdapCharField(db_column='lastname', object_classes=['genericPerson'])
+    user_password = LdapPasswordField(db_column='userpassword', object_classes=['genericPerson'])
+    nt_password = LdapNtPasswordField(db_column='ntpassword', object_classes=['genericPerson'])
+    display_name = LdapCharField(db_column='displayname', object_classes=['genericPerson'])
+    postal_address = LdapCharField(db_column='postaladdress', object_classes=['genericPerson'])
+
+    # reselPerson
+    inscr_date = LdapCharField(db_column='dateinscr', object_classes=['reselPerson'])
+    cotiz = LdapCharField(db_column='cotiz', object_classes=['reselPerson'])
+    end_cotiz = LdapCharField(db_column='endinternet', object_classes=['reselPerson'])
+    campus = LdapCharField(db_column='campus', object_classes=['reselPerson'])
+
+    # maiselPerson
+    building = LdapCharField(db_column='batiment', object_classes=['maiselPerson'])
+    room_number = LdapCharField(db_column='roomnumber', object_classes=['reselPerson'])
+    # # TODO: coupure
+    #
+    # # enstPerson
+    promo = LdapCharField(db_column='promo', object_classes=['enstbPerson'])
+    mail = LdapCharField(db_column='mail', object_classes=['enstbPerson'])
+    anneeScolaire = LdapCharField(db_column='anneeScolaire', object_classes=['enstbPerson'])
+    mobile = LdapCharField(db_column='telephoneNumber', object_classes=['enstbPerson'])
+    option = LdapCharField(db_column='option', object_classes=['enstbPerson'])
+    formation = LdapCharField(db_column='formation', object_classes=['enstbPerson'])
+    photo_file = LdapCharField(db_column='photoFile', object_classes=['enstbPerson'])
+    uid_godson = LdapListField(db_column='uidFillot', object_classes=['enstbPerson'])
+    uid_godfather = LdapListField(db_column='uidParrain', object_classes=['enstbPerson'])
+    origin = LdapCharField(db_column='provenance', object_classes=['enstbPerson'])
+    # # TODO : publiable
+    # # TODO : altmail
+    #
+    # # aePerson
+    ae_cotiz = LdapCharField(db_column='aeCotiz', object_classes=['aePerson'])
+    ae_nature = LdapCharField(db_column='aeNature', object_classes=['aePerson'])
+    n_adherent = LdapCharField(db_column='nAdherent', object_classes=['aePerson'])
+    # TODO: other fields
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+
+    @staticmethod
+    def from_json(json_input):
+        obj_dict = json.loads(json_input)
+        self = LdapUser()
+        for k, v in obj_dict.items():
+            self.__dict__[k] = v
+        return self
 
 
 class LdapPerson(ldapdb.models.Model):
--- gestion_personnes/tests.py
+++ gestion_personnes/tests.py
@@ -1,3 +1,113 @@
+import time
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
 
-# Create your tests here.
+from gestion_personnes.models import LUser
+from ldapback.models.fields import LdapCharField
+
+
+def try_delete_user(uid):
+    try:
+        user_s = LUser.get(pk=uid)
+        user_s.delete()
+        return True
+    except ObjectDoesNotExist:
+        return False
+
+
+class LdapuserTestCase(TestCase):
+    @staticmethod
+    def create_full_user():
+        user = LUser()
+        user.uid = "amanoury"
+        user.first_name = "Alexandre"
+        user.last_name = "Manoury"
+        user.user_password = "blah"
+        user.nt_password = "blah"
+        user.display_name = "Alexandre Manoury"
+        user.postal_address = "I11 Maisel blah\n blah blah"
+
+        user.inscr_date = time.strftime('%Y%m%d%H%M%S') + 'Z'
+        user.cotiz = "2016"
+        user.end_cotiz = time.strftime('%Y%m%d%H%M%S') + 'Z'
+        user.campus = "Brest"
+        user.building = "I11"
+        user.room_number = "123"
+
+        user.promo = "2020"
+        user.mail = "alexandre.manoury@telecom-bretagne.eu"
+        user.anneeScolaire = "2015"
+        user.mobile = "33676675525"
+        user.option = "Brest"
+        user.formation = "FIG"
+
+        user.ae_cotiz = "100"
+        user.ae_nature = "liquide"
+        user.n_adherent = "1235667"
+
+        return user
+
+    def test_new_user(self):
+        user = self.create_full_user()
+        try_delete_user(user.uid)
+        user.save()
+
+        user_s = LUser.get(pk=user.uid)
+        self.assertIsInstance(user_s, LUser)
+        self.assertEqual(user_s.uid, "amanoury")
+        self.assertEqual(user_s.first_name, "Alexandre")
+        self.assertEqual(user_s.last_name, "Manoury")
+        self.assertEqual(user_s.display_name, "Alexandre Manoury")
+        self.assertEqual(user_s.postal_address, "I11 Maisel blah\n blah blah")
+        self.assertEqual(user_s.inscr_date, user.inscr_date)
+        self.assertEqual(user_s.cotiz, "2016")
+        self.assertEqual(user_s.end_cotiz, user.end_cotiz)
+        self.assertEqual(user_s.campus, "Brest")
+        self.assertEqual(user_s.building, "I11")
+        self.assertEqual(user_s.room_number, "123")
+        self.assertEqual(user_s.promo, "2020")
+        self.assertEqual(user_s.mail, "alexandre.manoury@telecom-bretagne.eu")
+        self.assertEqual(user_s.anneeScolaire, "2015")
+        self.assertEqual(user_s.mobile, "33676675525")
+        self.assertEqual(user_s.option, "Brest")
+        self.assertEqual(user_s.formation, "FIG")
+        self.assertEqual(user_s.ae_cotiz, "100")
+        self.assertEqual(user_s.ae_nature, "liquide")
+        self.assertEqual(user_s.n_adherent, "1235667")
+
+        try_delete_user(user.uid)
+
+    def test_only_ResEl(self):
+        user = LUser()
+        user.uid = "amanoury"
+        user.first_name = "Alexandre"
+        user.last_name = "Manoury"
+        user.user_password = "blah"
+        user.nt_password = "blah"
+
+        user.display_name = "Alexandre Manoury"
+        user.postal_address = "I11 Maisel blah\n blah blah"
+
+        user.inscr_date = time.strftime('%Y%m%d%H%M%S') + 'Z'
+        user.cotiz = "2016"
+        user.end_cotiz = time.strftime('%Y%m%d%H%M%S') + 'Z'
+
+        try_delete_user(user.uid)
+        user.save()
+
+        user_s = LUser.get(pk=user.uid)
+        self.assertIsInstance(user_s, LUser)
+        self.assertEqual(user.uid, user_s.uid)
+        self.assertIsInstance(user.building, LdapCharField)
+
+        # Now lets add a maisel field :
+        user_s.building = "I11"
+        user_s.room_number = "203"
+        user_s.save()
+
+        user_t = LUser.get(pk=user.uid)
+        self.assertIsInstance(user_t, LUser)
+        self.assertEqual(user_t.building, "I11")
+        self.assertEqual(user_t.room_number, "203")
+
