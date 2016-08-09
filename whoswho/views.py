c987456335ff3bcf56b7affce949685028717209
Quentin Jodelet <magicflottant@gmail.com>
2016-08-09 11:12:26 +0200

    Whoswho update

--- whoswho/views.py
+++ whoswho/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.views.generic import View
-from .models import UserModel
+from django.http import Http404
+from gestion_personnes.models import LdapUser
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 class Godparents(View):
     """
@@ -24,7 +29,29 @@ class UserDetails(View):
     
     template_name = 'whoswho/userDetails.html'
     
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(UserDetails, self).dispatch(*args, **kwargs)
+        
     def get(self, request, uid):
-        user = UserModel.get(uid=uid)
+        try:
+            user = LdapUser.get(uid=uid)
+        except ObjectDoesNotExist:
+            raise Http404
+        
+        user.godchildren = []
+        for line in user.uid_godchildren:
+            try:
+                user.godchildren.append(LUser.get(line[4:find(line,',')]))
+            except ObjectDoesNotExist:
+                pass
+            
+        user.godparents = []
+        for line in user.uid_godparents:
+            try:
+                user.godparents.append(LUser.get(line[4:find(line,',')]))
+            except ObjectDoesNotExist:
+                pass
+        
         return render(request, self.template_name, {'user' : user})
     
