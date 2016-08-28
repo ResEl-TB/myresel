d3084af3e8aebe2213621549f943c35b8a117d44
Dimtion <zizou.xena@gmail.com>
2016-08-28 14:56:16 +0200

    Update network config for Rennes

--- fonctions/generic.py
+++ fonctions/generic.py
@@ -1,6 +1,9 @@
-import binascii, hashlib, os
+import binascii
+import hashlib
+import os
 from base64 import encodestring
 from datetime import datetime, timedelta
+
 
 def current_year():
     """ Fonction qui calcule l'année scolaire en cours """
@@ -12,6 +15,7 @@ def current_year():
         year -= 1
 
     return year
+
 
 def get_end_date(duree):
     """ Fonction qui renvoie la date actuelle + la durée en jours """
@@ -28,16 +32,19 @@ def hash_passwd(password):
     h.update(salt)
     return "{SSHA}" + str(encodestring(h.digest() + salt)).split("'")[1].split('\\')[0]
 
+
 def hash_to_ntpass(password):
     """ Fourni un hash du mdp pour correspondre avec le loggin over Wi-Fi """
 
     return str(binascii.hexlify(hashlib.new('md4', password.encode('utf-16le')).digest()).upper()).split('\'')[1]
+
 
 def ip_to_int(ip):
     """ Convert an IP in int"""
     o = list(map(int, ip.split('.')))
     res = (16777216 * o[0]) + (65536 * o[1]) + (256 * o[2]) + o[3]
     return res
+
 
 def is_ip_in_subnet(ip, ipNetwork, maskLength):
     """ Check if ip is in the subnet ipNetwork/maskLength"""
