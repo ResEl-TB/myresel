a4cdb58ff802d2b64fefaf619f10c2c1aa15eb21
Quentin Jodelet <magicflottant@gmail.com>
2016-08-29 19:32:58 +0200

    Correction Pour loic : il y a un bug dans la fonction save() de LdaptUser, de plus les set ne semble pas fonctionner car tu n'as pas surcharger l'operateur == de LdaptUser

--- whoswho/views.py
+++ whoswho/views.py
@@ -117,12 +117,16 @@ class AddBizu(View):
     
     def post(self, request, *args, **kwargs):      
         try:
-            godchild = LdapUser.get(uid=request.POST.get('id_utilisateur', ''))
+            godchild = LdapUser.get(uid=request.POST.get('id_user', ''))
         except ObjectDoesNotExist:
             messages.error(request, _("L'utilisateur que vous souhaitez ajouter comme filleul n'existe pas."))
             return HttpResponseRedirect(reverse('who:user-details', args=[request.user.username]))
         
         godparent = LdapUser.get(uid=request.user.username)
+        
+        if godparent.pk == godchild.pk:
+            messages.success(request, _("Vous ne pouvez pas vous ajouter vous même."))
+            return HttpResponseRedirect(reverse('who:user-details', args=[request.user.username]))
         
         if not godchild.pk in godparent.uid_godchildren : 
             godparent.uid_godchildren.append(godchild.pk)
