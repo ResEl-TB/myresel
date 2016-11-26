526a8f0bc8a269ded47e6e3dc1266032cc1f3408
Dimtion <zizou.xena@gmail.com>
2016-11-26 14:18:14 +0100

    Configure QoS structure in installation script

--- .install/scripts/configure.sh
+++ .install/scripts/configure.sh
@@ -2,10 +2,18 @@
 
 if [ ! -f ${CONFDIR}settings_local.py ]; then
     cp ${CONFDIR}settings_local.py.tpl ${CONFDIR}settings_local.py
+
     sed -i "/LDAP_PASSWD *=/s/ *=.*/ = \"${LDAP_PASSWORD}\"/" ${CONFDIR}settings_local.py
+
     sed -i "/DB_NAME *=/s/ *=.*/ = \"${MYSQL_DATABASE}\"/" ${CONFDIR}settings_local.py
     sed -i "/DB_HOST *=/s/ *=.*/ = \"${MYSQL_HOST}\"/" ${CONFDIR}settings_local.py
     sed -i "/DB_USER *=/s/ *=.*/ = \"${MYSQL_USER}\"/" ${CONFDIR}settings_local.py
     sed -i "/DB_PASSWORD *=/s/ *=.*/ = \"${MYSQL_PASSWORD}\"/" ${CONFDIR}settings_local.py
+
+    sed -i "/DB_QOS_NAME *=/s/ *=.*/ = \"${MYSQL_QOS_DATABASE}\"/" ${CONFDIR}settings_local.py
+    sed -i "/DB_QOS_HOST *=/s/ *=.*/ = \"${MYSQL_QOS_HOST}\"/" ${CONFDIR}settings_local.py
+    sed -i "/DB_QOS_USER *=/s/ *=.*/ = \"${MYSQL_QOS_USER}\"/" ${CONFDIR}settings_local.py
+    sed -i "/DB_QOS_PASSWORD *=/s/ *=.*/ = \"${MYSQL_QOS_PASSWORD}\"/" ${CONFDIR}settings_local.py
+
     sed -i "/REDIS_HOST *=/s/ *=.*/ = \"${REDIS_HOST}\"/" ${CONFDIR}settings_local.py
 fi
