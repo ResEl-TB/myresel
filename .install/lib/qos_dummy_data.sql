49525ba2e885c070b82194da4a8f65358d9aaedb
Dimtion <zizou.xena@gmail.com>
2016-12-07 04:48:50 +0100

    Generate dummy data for qos database

---
+++ .install/lib/qos_dummy_data.sql
@@ -0,0 +1,77 @@
+-- Insert Dummy data in QoS database
+
+INSERT INTO people_data VALUES (
+    'brest',
+    'uid=lcarr,ou=people,dc=maisel,dc=enst-bretagne,dc=fr',
+    'lcarr',
+    'down',
+    'clean',
+    0,
+    5698119055,
+    56983721,
+    5234
+);
+
+INSERT INTO people_data VALUES (
+    'brest',
+    'uid=lcarr,ou=people,dc=maisel,dc=enst-bretagne,dc=fr',
+    'lcarr',
+    'down',
+    'dirty',
+    0,
+    5698119055,
+    56983721,
+    5234
+);
+
+
+INSERT INTO people_data VALUES (
+    'brest',
+    'uid=lcarr,ou=people,dc=maisel,dc=enst-bretagne,dc=fr',
+    'lcarr',
+    'up',
+    'clean',
+    0,
+    5698119055,
+    56983721,
+    5234
+);
+INSERT INTO people_data VALUES (
+    'brest',
+    'uid=lcarr01,ou=people,dc=maisel,dc=enst-bretagne,dc=fr',
+    'lcarr01',
+    'down',
+    'clean',
+    0,
+    5698119055,
+    56983721,
+    5234
+);
+
+INSERT INTO people_data VALUES (
+    'brest',
+    'uid=lcarr01,ou=people,dc=maisel,dc=enst-bretagne,dc=fr',
+    'lcarr01',
+    'down',
+    'dirty',
+    0,
+    5698119055,
+    56983721,
+    5234
+);
+
+
+INSERT INTO people_data VALUES (
+    'brest',
+    'uid=lcarr01,ou=people,dc=maisel,dc=enst-bretagne,dc=fr',
+    'lcarr01',
+    'up',
+    'clean',
+    0,
+    5698119055,
+    56983721,
+    5234
+);
+
+-- Dummy data history
+
