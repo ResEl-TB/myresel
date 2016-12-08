a2ceaa376a9f02fb428cb096c18e0776dca1e536
Morgan ROBIN <mrobin@robin.wf>
2016-12-08 18:10:22 +0100

    Update models for room management

--- campus/submodels/rooms_models.py
+++ campus/submodels/rooms_models.py
@@ -1,26 +1,37 @@
 from django.db import models
 from django.core.mail import EmailMessage
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import Group
+from django.db.models import Q
 
+import django_rq
 from datetime import timedelta
 
 from gestion_personnes.models import LdapUser
+import campus.async_tasks as async_tasks
 
 class Room(models.Model):
+    class Meta:
+        verbose_name = _('salle')
+
     LOCATIONS = (
         ('F', _('Foyer')),
         ('S', _('École')),
+        ('O', _('Extérieur')),
+        ('C', _('Campus')),
     )
 
     location = models.CharField(
         max_length=1,
         choices=LOCATIONS,
         help_text=_('emplacement de la salle'),
+        verbose_name=_('Emplacement'),
     )
 
     name = models.CharField(
         max_length=20,
         help_text=_('nom de la salle'),
+        verbose_name=_('Nom de la salle'),
     )
 
     mailing_list = models.EmailField(
@@ -31,33 +42,39 @@ class Room(models.Model):
     private = models.BooleanField(
         help_text=_('indique si la salle est privée ou non'),
         default=False,
+        verbose_name=_('Salle privée / club'),
     )
+
+    allowed_members = models.TextField(
+        help_text=_('indique les utilisateurs pouvant réserver la salle, un par ligne'),
+        blank=True,
+        verbose_name=_('Membres authorisés'),
+    )
+
+    def __str__(self):
+        return '%s - %s' % (self.name, self.get_location_display()) 
 
     def is_free(self, start_date, end_date):
         """ Checks if a room is free between the time range given """
-        events = self.roombooking_set.all() \
-            .filter(start_time__range=(start_date, start_date + timedelta(days=1)))
-        free1 = True
-        for e in events:
-            if e.start_time <= start_date <= e.end_time:
-                free1 = False
-                break
+        
+        events = RoomBooking.objects.filter(
+            room__pk=self.pk,
+            start_time__year=start_date.year,
+            start_time__month=start_date.month,
+            start_time__day=start_date.day,
+        )
+        print(events)
 
-        events = self.roombooking_set.all() \
-            .filter(end_time__range=(end_date, end_date + timedelta(days=1)))
-        free2 = True
-        for e in events:
-            if e.start_time <= end_date <= e.end_time:
-                free2 = False
+        free = True
+        for event in events:
+            if event.start_time <= start_date <= event.end_time or event.start_time <= end_date <= event.end_time:
+                free = False
                 break
-
-        return free1 and free2
+        return free
 
 class RoomBooking(models.Model):
-    class META:
-        permissions = (
-            ('can_moderate', 'Indique si l\'admin peut modérer ou non les réservations de salle'),
-        )
+    class Meta:
+        verbose_name = _('réservation')
 
     BOOKING_TYPES = (
         ('party', _('Soirée')),
@@ -71,11 +88,6 @@ class RoomBooking(models.Model):
         ('other', _('Autre')),
         ('hidden', _('Ne pas afficher sur le calendrier')),
     )
-    
-    name = models.CharField(
-        max_length=20,
-        help_text=_('nom de l\'évènement'),
-    )
 
     description = models.TextField(
         help_text=_('description de l\'évènement'),
@@ -84,72 +96,56 @@ class RoomBooking(models.Model):
     room = models.ManyToManyField(
         'Room',
         help_text=_('indique dans quelle salle se déroule l\'évènement'),
+        verbose_name=_('Salle(s)'),
     )
 
     start_time = models.DateTimeField(
         help_text=_('début de l\'évènement'),
+        verbose_name=_('Début de l\'evènement'),
     )
 
     end_time = models.DateTimeField(
         help_text=_('fin de l\'évènement'),
+        verbose_name=_('Fin de l\'évènement'),
     )
 
     user = models.CharField(
         max_length=50,
         help_text=_('utilisateur ayant fait la demande de réservation'),
+        verbose_name=_('Utilisateur'),
     )
 
     booking_type = models.CharField(
         max_length=50,
         choices=BOOKING_TYPES,
         help_text=_('type d\'évènement'),
+        verbose_name='Type', 
     )
 
     displayable = models.BooleanField(
         help_text=_('affiche ou non cet évènement sur le calendrier'),
         default=True,
-    )
-
-    moderated = models.BooleanField(
-        help_text=_('indique si l\'évènement a été modéré'),
-        default=False,
-        editable=False,
-    )
-
-    moderated_by = models.CharField(
-        max_length=50,
-        help_text=_('indique l\'admin qui a modéré l\'évènemnt'),
-        default='pas encore modéré',
-        editable=False,
+        verbose_name=_('Affichable'),
     )
 
     def __str__(self):
-        return self.name
+        return '%s - %s' % (self.description, self.start_time.strftime('Début le %d %B %Y à %H:%M'))
 
     def notify_mailing_list(self):
-        user = LdapUser.get(pk=self.user)
+        queue = django_rq.get_queue()
         for room in self.room.all():
             if room.location == 'S':
-                mail = EmailMessage(
-                    subject='Réservation de la salle %s' % room.name,
-                    body=('Bonjour,\n\n' +
-                          'Ceci est un mail automatique envoyé par le site de réservation de salles du ResEl.\n\n' +
-                          'L\'utilisateur %(user)s souhaiterait réserver une salle selon ces informations :\n' +
-                          '- salle : %(room)s\n' +
-                          '- date de début : %(start_date)s\n' +
-                          '- date de fin : %(end_date)s\n\n' +
-                          'Vous pouvez contacter cette personne directement en réponse à ce mail.') % {
-                                                                                                        'user': user.display_name,
-                                                                                                        'room': room.name,
-                                                                                                        'start_date': self.start_time,
-                                                                                                        'end_date': self.end_time,
-                                                                                                        },
-                    from_email='noreply@resel.fr',
-                    reply_to=[user.mail],
-                    to=[room.mailing_list],
+                queue.enqueue_call(
+                    async_tasks.notify_mailing_list,
+                    args=(LdapUser.get(pk=self.user), room),
                 )
-                mail.send()
 
-    def notify_moderators(self):
-        # TODO
-        pass
+class RoomAdmin(models.Model):
+    class Meta:
+        verbose_name = 'administrateur des salles'
+        verbose_name_plural = 'administrateurs des salles'
+
+    uid = models.CharField(
+        max_length=15,
+        verbose_name='nom d\'utilisateur',
+    )
