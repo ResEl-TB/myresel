f86da6acd35ae943d039b52b390a3df3088384a0
Morgan ROBIN <mrobin@robin.wf>
2017-01-24 18:09:35 +0100

    Add the LdapGroup model

--- campus/models.py
+++ campus/models.py
@@ -1,3 +1,18 @@
 from campus.submodels.rooms_models import *
 from campus.submodels.mails_models import *
 from campus.submodels.clubs_models import *
+
+import ldapback
+from ldapback.models.fields import LdapCharField, LdapListField
+from myresel.settings import LDAP_DN_GROUPS
+
+class LdapGroup(ldapback.models.LdapModel):
+    """
+    The class managing the groups in the LDAP
+    """
+
+    base_dn = LDAP_DN_GROUPS
+    object_classes = ['groupOfNames']
+
+    cn = LdapCharField(db_column='cn', object_classes=['groupOfNames'], pk=True)
+    members = LdapListField(db_column='member', object_classes=['groupOfNames'])
