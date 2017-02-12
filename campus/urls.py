e8859fcbddac8cd83ab7b960cd81561c2f7dd000
Dimtion <zizou.xena@gmail.com>
2017-02-12 21:35:15 +0100

    Create link on home page for campus

--- campus/urls.py
+++ campus/urls.py
@@ -2,7 +2,10 @@
 
 from django.conf.urls import url, include
 
+from campus.views import home_view
+
 urlpatterns = [
+    url(r'^$', home_view, name='home'),
     url(r'^salles/', include('campus.suburls.urls_rooms', namespace='rooms')),
     url(r'^mails/', include('campus.suburls.urls_mails', namespace='mails')),
     url(r'^who/', include('campus.whoswho.urls', namespace='who')),
--- campus/views.py
+++ campus/views.py
@@ -1,2 +1,6 @@
 from campus.subviews.views_rooms import *
 from campus.subviews.views_mails import *
+
+
+def home_view(request):
+    return HttpResponseRedirect(reverse("campus:rooms:calendar"))
