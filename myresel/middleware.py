ResEl-TB/myresel

commit 1e867f57347850ce59f0cf6861a62d19b38bd024

Add metadata to loggers

myresel/middleware.py
@@ -18,8 +18,8 @@ class IWantToKnowBeforeTheRequestIfThisUserDeserveToBeAdminBecauseItIsAResElAdmi
     def make_user_staff(username):
         """
         Save in Django database if a user is part of the staff
-        :param username: 
-        :return: 
+        :param username:
+        :return:
         """
         user = User.objects.get(username=username)
         user.is_staff = 1
@@ -98,7 +98,7 @@ class NetworkConfiguration(object):
 
 class InscriptionNetworkHandler(object):
     """
-    Before the request is sent to the website, we need to handle if the user 
+    Before the request is sent to the website, we need to handle if the user
     is in an inscription network
     """
     def __init__(self, get_response):
@@ -145,7 +145,14 @@ class InscriptionNetworkHandler(object):
                              "\n VLAN : %s"
                              "\n Utilisateur : %s"
                              "\n Machine : %s"
-                             % (ip, host, zone, vlan, is_logged_in, is_registered))
+                             % (ip, host, zone, vlan, is_logged_in, is_registered),
+                             extra={
+                                 "device_ip": ip,
+                                 "device_hostname": host,
+                                 "device_zone": zone,
+                                 "device_vlan": vlan
+                                 "user": is_logged_in
+                             })
 
                 # Error ! In vlan 995 without inscription IP address
                 return HttpResponseBadRequest(_("Vous vous trouvez sur un réseau d'inscription mais ne possédez pas d'IP dans ce réseau. Veuillez contacter un administrateur."))

devices/views.py
@@ -202,7 +202,8 @@ class EditDeviceView(View):
                            "\n\nuid: {uid}"
                            "\nhostname: {hostname}".format(
                 uid=request.user.username,
-                hostname=hostname)
+                hostname=hostname),
+                           extra={"uid": request.user.username, "device_hostname": hostname},
             )
             messages.error(request, _("Cette machine n'existe pas ou ne vous appartient pas."))
             return HttpResponseRedirect(reverse('gestion-machines:liste'))
@@ -230,7 +231,8 @@ class EditDeviceView(View):
                                "\nnew alias: {alias}".format(
                     uid=request.user.username,
                     alias=alias,
-                    hostname=hostname)
+                    hostname=hostname),
+                               extra={"uid": request.user.username, "new_alias": alias, "device_hostname": hostname}
                 )
                 messages.error(request, _("Cette machine n'existe pas ou ne vous appartient pas."))
                 return HttpResponseRedirect(reverse('gestion-machines:liste'))

pages/views.py
@@ -197,11 +197,11 @@ class Contact(View):
 def inscription_zone_info(request):
     """
     View which explains the users on how to register to the ResEl
-    
+
     If the device is not registered show the information
-    If the device is simply not active, it redirect the user to the 
+    If the device is simply not active, it redirect the user to the
     reactivation view
-    :param request: 
+    :param request:
     :return: HttpResponse
     """
     zone = request.network_data['zone']
@@ -214,7 +214,9 @@ def inscription_zone_info(request):
             if not is_registered:
                 return HttpResponseRedirect(reverse("gestion-machines:reactivation"))
         except ObjectDoesNotExist:
-            logger.warning("The device with the mac %s was not found in the LDAP" % mac)
+            logger.warning("The device with the mac %s was not found in the LDAP" % mac,
+                    extra={"device_mac": mac}
+            )
 
     if "inscription" not in zone:
         return HttpResponseRedirect(reverse("home"))

tresorerie/views.py
@@ -171,7 +171,11 @@ class Pay(View):
             token = request.POST['stripeToken']
             given_uuid = uuid.UUID(request.POST['uuid'])
         except MultiValueDictKeyError:
-            logger.error("Un utilisateur n'a pas pu payer car Stripe ne s'est pas chargé,\n pika check le fw ;)")
+            logger.error("Un utilisateur n'a pas pu payer car Stripe ne s'est pas chargé,\n pika check le fw ;)",
+                    extra={
+                        "transaction_uuid": transaction_uuid,
+                        "uid": user.uid
+                    })
             messages.error(request, _("Il semblerait que nous n'avons pas réussi à contacter le système de paiement. Si le problème se reproduit vous pouvez le contouner soit en essayant de payer depuis une connexion exterieur (depuis l'école ou en 4G), soit en nous contactant directement."))
             return HttpResponseRedirect(reverse('tresorerie:pay', kwargs={'product_id': main_product_id}))
 
@@ -183,12 +187,14 @@ class Pay(View):
 
         if given_uuid != transaction_uuid:
             # TODO: show error message
-            logger.warning("L'uuid d'une transaction n'est pas correcte. donnée : %s attendue : %s" % (given_uuid, transaction_uuid))
+            logger.warning("L'uuid d'une transaction n'est pas correcte. donnée : %s attendue : %s" % (given_uuid, transaction_uuid),
+                    extra={"given_uuid": given_uuid, "transaction_uuid": transaction_uuid})
             messages.error(request, _("Une erreur s'est produite lors de la commande. Les administrateurs en ont été informés"))
             return HttpResponseRedirect(reverse('tresorerie:pay', kwargs={'product_id': main_product_id}))
 
         if adhere and user.is_member():
-            logger.warning("L'utilisateur %s a tenté de payer à nouveau une cotisation, une magouille s'est produite" % user.uid)
+            logger.warning("L'utilisateur %s a tenté de payer à nouveau une cotisation, une magouille s'est produite" % user.uid,
+                    extra={"uid": user.uid})
             messages.error(request, _("Vous êtes déjà membre de l'association, vous n'avez pas besoin de payer à nouveau la cotisation."))
             return HttpResponseRedirect(reverse('tresorerie:pay', kwargs={'product_id': main_product_id}))
 
@@ -235,7 +241,12 @@ class Pay(View):
                 async_tasks.generate_and_email_invoice,
                 args=(request.ldap_user, transaction, get_language()),
             )
-            logger.info("Paiement validé par le système, uid: %s, uuid: %s, stripe id : %s" %(request.ldap_user.uid, transaction.uuid, transaction.stripe_id))
+            logger.info("Paiement validé par le système, uid: %s, uuid: %s, stripe id : %s" %(request.ldap_user.uid, transaction.uuid, transaction.stripe_id),
+                    extra={
+                        "uid": request.ldap_user.uid,
+                        "transaction_uuid": transaction.uuid,
+                        "transaction_stripe_id": transaction.stripe_id
+                    })
             messages.success(request, _("Vous venez de payer votre accès au ResEl, vous devriez recevoir sous peu un email avec votre facture."))
             return HttpResponseRedirect(reverse('tresorerie:historique'))
 
@@ -252,7 +263,8 @@ class Pay(View):
                 'card_declined': _("Votre carte a été refusée"),
                 'processing_error': _("Une erreur est survenue dans le traitement de votre demande")
             }
-            logger.warning("Carte de crédit non valide, erreur : %s, uid : %s" % (ERRORS[code], request.ldap_user.uid))
+            logger.warning("Carte de crédit non valide, erreur : %s, uid : %s" % (ERRORS[code], request.ldap_user.uid),
+                    extra={"error_code": ERRORS[code], "uid": request.ldap_user.uid})
             messages.error(request, ERRORS[code])
             return HttpResponseRedirect(reverse('tresorerie:pay', kwargs={'product_id': main_product_id}))
 

commit b7b45f676a09c98da9adce21c25e3687b6ffadba

Solve syntax error introduced in 1e867f57347850ce59f0cf6861a62d19b38bd024

myresel/middleware.py
@@ -150,7 +150,7 @@ class InscriptionNetworkHandler(object):
                                  "device_ip": ip,
                                  "device_hostname": host,
                                  "device_zone": zone,
-                                 "device_vlan": vlan
+                                 "device_vlan": vlan,
                                  "user": is_logged_in
                              })
 