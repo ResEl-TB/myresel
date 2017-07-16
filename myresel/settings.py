f8927be2274418634c93db3a4608f73455ef3da6
Dimtion <zizou.xena@gmail.com>
2017-07-16 23:28:20 +0200

    Revert "Revert "Add REDIS caching and sessions""
    This reverts commit dda189e48836c1ef6fab334ab3e922a2f883e57c.
    
    This commit was bloqued in production by :
    https://git.resel.fr/resel/general/issues/92
    
    So we recreate it, but until this issue is not solved we do not merge to
    `deploy`

--- myresel/settings.py
+++ myresel/settings.py
@@ -161,6 +161,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -174,6 +175,7 @@ MIDDLEWARE = [
     'myresel.middleware.IWantToKnowBeforeTheRequestIfThisUserDeserveToBeAdminBecauseItIsAResElAdminSoCheckTheLdapBeforeMiddleware',
     'myresel.middleware.NetworkConfiguration',
     'myresel.middleware.InscriptionNetworkHandler',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'myresel.urls'
@@ -210,6 +212,7 @@ DATABASES = {
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
         'HOST': DB_HOST,
+        'CONN_MAX_AGE': 60 * 10  # 10 minutes de connexion maia
     },
 }
 
@@ -282,6 +285,30 @@ RQ_QUEUES = {
    },
 }
 RQ_SHOW_ADMIN_LINK = True
+
+####
+# CACHE configuration
+####
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://%s:%s/%s" % (REDIS_HOST, REDIS_PORT, REDIS_DB),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": REDIS_PASSWORD,
+        }
+    }
+}
+CACHE_MIDDLEWARE_KEY_PREFIX = 'myresel_cache_'
+
+####
+# SESSIONS management
+####
+
+# Use redis as session cache
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
 
 
 ####
@@ -377,6 +404,21 @@ DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda request: True,
     'SHOW_COLLAPSED': True,
 }
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
 
 if DEBUG and not TESTING:
     INSTALLED_APPS += ['debug_toolbar',]
