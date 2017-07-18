392f42fc3f4e4c0c6c3cb99e2e31ebf7427589a2
Dimtion <zizou.xena@gmail.com>
2017-07-18 19:27:19 +0200

    Change \n by " " status_rqworker; icinga is nazi

--- myresel/management/commands/status_rqworker.py
+++ myresel/management/commands/status_rqworker.py
@@ -17,23 +17,23 @@ class Command(BaseCommand):
         ]
         regex = re.compile(r"^active")
         is_error = False
-        error_output = "MYRESEL CRITICAL:\n"
+        error_output = "MYRESEL CRITICAL: "
 
         for service in services:
             try:
                 out = subprocess.check_output(["systemctl", "is-active", service]).decode()
+                if not re.match(regex, out):
+                    is_error = True
+                    error_output += "%s is %s; " % (service, out)
             except FileNotFoundError as e:
                 is_error = True
-                error_output += "Cannot execute systemctl is-active %s\n" % service
+                error_output += "Can't execute 'systemctl is-active %s'; " % service
             except subprocess.CalledProcessError as e:
                 is_error = True
-                error_output += "%s is %s\n" % (service, e.output.decode().strip())
-
-            if not re.match(regex, out):
-                is_error = True
-                error_output += "%s is %s\n" % (service, out)
+                error_output += "%s is %s; " % (service, e.output.decode().strip())
 
         if is_error:
             raise CommandError(error_output)
         else:
             self.stdout.write(self.style.SUCCESS('MYRESEL OK: All services running'))
+
