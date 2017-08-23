5cea646f68a243d59a7bdf93bed0f9d35e5a6cb6
JB Valladeau <jbvalladeau@gmail.com>
2017-08-23 23:13:38 +0200

    Fixes few mistakes in Club views

--- campus/views/views_clubs.py
+++ campus/views/views_clubs.py
@@ -264,7 +264,7 @@ class AddPersonToClub(View):
 
     def add_user(self, user, club, request):
         """
-        Add a `user` to a `club` and subscribe him to the mlist, 
+        Add a `user` to a `club` and subscribe him to the mlist,
         no verification is made.
         """
         club.members.append(user.pk)
@@ -323,7 +323,7 @@ class AddPersonToClub(View):
                     raise Http404("L'utilisateur n'existe pas")
 
         if not user.pk in club.members and (("tbClub" in club.object_classes or \
-        "tbCampagne" in club.object_classes or "tbClubSport in club.object_classes") or \
+        "tbCampagne" in club.object_classes or "tbClubSport" in club.object_classes) or \
         (self.request.ldap_user.is_campus_moderator() or self.request.ldap_user.pk in club.prezs or\
         request.user.is_staff)): # Our ldap is crap
             self.add_user(user, club, request)
@@ -339,7 +339,7 @@ class AddMailToClub(View):
     View used to add a person to a specific club/list or asso if he's got the right to do so
     """
 
-    def get(self, request, pk):
+    def post(self, request, pk):
         try:
             club=StudentOrganisation.get(cn=pk)
             if "tbCampagne" in club.object_classes:
@@ -371,7 +371,15 @@ class AddMailToClub(View):
         try:
             subscription_email.send()
         except SMTPException:
-            print("Somthing went wrong when trying to send club subscription message")
+            logger.warning(
+                "Erreur lors de la désinscription de la mlist %s de %s" % (mail, user.mail),
+                extra={
+                    'uid': user.uid,
+                    'user_mail': user.mail,
+                    'mlist': mail,
+                    'message_code': 'ERROR_UNSUBSCRIBE',
+                }
+            )
 
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
@@ -404,16 +412,12 @@ class AddPrezToClub(View):
         except ObjectDoesNotExist:
             raise Http404("L'utilisateur n'éxiste pas")
 
-        if "tbClub" in club.object_classes and not user.pk in club.prezs:
-            club.prezs = [user.pk]
-            club.save()
-            messages.success(request, _("Le président viens d'être ajouté"))
-        else:
-            messages.info(request, _("Cette personne est déjà président(e)"))
-
-        if "tbClub" in club.object_classes and not user.pk in club.members:
+        club.prezs = [user.pk]
+        if not user.pk in club.members:
             club.members.append(user.pk)
-            club.save()
+        club.save()
+
+        messages.success(request, _("Le président viens d'être ajouté"))
 
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
