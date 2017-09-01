cb14b49929d8a7d2c2d997bcf7db37bb72c2f803
Loïc CARR <zizou.xena@gmail.com>
2017-09-02 00:14:56 +0200

    Create the become administrator page

--- myresel/urls.py
+++ myresel/urls.py
@@ -62,6 +62,7 @@ urlpatterns = [
     url(r'^favicon.ico', RedirectView.as_view(url='/static/images/icons/favicon-96x96.png', permanent=False), name="favicon"),
 
     url(r'^help-needed/', TemplateView.as_view(template_name='help_needed.html'), name="help-needed"),
+    url(r'^become/$', TemplateView.as_view(template_name='pages/become_admin.html'), name='become-admin'),
 
     url(r'^$', Home.as_view(), name='home'),
 ]
