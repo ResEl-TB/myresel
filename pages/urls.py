23ca297fc5fb495b1cd4aaf9c93662294eff8418
Loïc CARR <zizou.xena@gmail.com>
2018-02-18 21:01:22 +0100

    Create eggdrop page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 
 from django.conf.urls import url
-from pages.views import NewsListe, FaqList, faqVote, NewsDetail, unsecure_set_language
+from pages.views import NewsListe, FaqList, faqVote, NewsDetail, unsecure_set_language, eggdrop
 
 urlpatterns = [
     url(r'^faq/$', FaqList.as_view(), name='faq'),
     url(r'^faq/upvote/$', faqVote, name='upvote'),
+    url(r'^eggdrop/$', eggdrop, name='eggdrop'),
     ]
